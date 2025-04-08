6c78f9ccb684d167ed600c08b1e8787464753e03
Yanis-Ourir <ourir.yanis@gmail.com>
2025-04-08 23:22:08 +0200

    updated twitter api and env

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -6,21 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategoryRepository;
+use App\Repository\LinkRepository;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Service\FormatApiTwitter;
 
+
+#[IsGranted('ROLE_USER')]
 final class FeedController extends AbstractController
 {
     #[Route('/feed', name: 'app_feed')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, FormatApiTwitter $formatApi, LinkRepository $linkRepository): Response
     {
         $user = $this->getUser();
-        if (!$user) {
-            return $this->redirectToRoute('app_login');
-        }
         $categories = $categoryRepository->findBy(['owner' => $user]);
+        $posts = [];
+        $links = $linkRepository->findAll();
+        foreach ($links as $link) {
+            $posts[] = [
+                'title' => $link->getTitle(),
+                'url' => $link->getUrl(),
+                'createdAt' => $link->getCreatedAt(),
+                'updatedAt' => $link->getUpdatedAt(),
+            ];
+        }
+
+        dd($posts);
         
         return $this->render('feed/index.html.twig', [
             'controller_name' => 'FeedController',
             'categories' => $categories,
+            'posts' => $posts,
         ]);
     }
 }
--- src/Service/FormatApiTwitter.php
+++ src/Service/FormatApiTwitter.php
@@ -3,6 +3,7 @@ namespace App\Service;
 
 use App\Entity\Link;
 use App\Repository\LinkRepository;
+use GuzzleHttp\Client;
 
 class FormatApiTwitter implements FormatApiDataInterface
 {
@@ -10,30 +11,22 @@ class FormatApiTwitter implements FormatApiDataInterface
 
     public function ApiCall(string $apiLink): void
     {
-        // Implement the API call logic here
-        // For example, you can use GuzzleHttp or cURL to make the request to the Twitter API
-        // and then process the response using the format method.
-        // Example of using GuzzleHttp:
-        // $client = new \GuzzleHttp\Client();
-        // $response = $client->request('GET', $apiLink, [
-        //     'headers' => [
-        //      
-        $ch = curl_init($apiLink);
-        $fp = fopen('response.json', 'w+');
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        } else {
-            echo 'Success!';
-        }
-        
-        curl_close($ch);
-        fclose($fp);
+        dd($apiLink);
+        $client = new Client();
+        $response = $client->request('GET', $apiLink, [
+            'header' => [
+                'Authorization' => 'Bearer ' + $_ENV('ACCESS_TOKEN_TWITTER'),
+                'client_id' => $_ENV('CLIENT_ID_TWITTER'),
+                'client_secret' => $_ENV('CLIENT_SECRET_TWITTER'),
+            ]
+        ]);
+        $statusCode = $response->getStatusCode();
+        $body = $response->getBody();
+        $json = json_decode($body, true);
+        $this->format($json);
     }
 
-    public function format($data): array
+    public function format(array $data): array
     {
      
 
@@ -47,13 +40,24 @@ class FormatApiTwitter implements FormatApiDataInterface
         //     }
         // ] JSON
 
-        $testData = [
-                    'text' => 'lucina book https://t.co/d7cVubXSli',
-                    'id' => '1908907475069538317',
-                    'edit_history_tweet_ids' => [
-                        '1908907475069538317'
-                    ]
-                ];
+        $testData = json_encode([
+            'data' => [
+            [
+                'text' => 'lucina book https://t.co/d7cVubXSli',
+                'id' => '1908907475069538317',
+                'edit_history_tweet_ids' => [
+                '1908907475069538317'
+                ]
+            ],
+            [
+                'text' => 'another tweet',
+                'id' => '1908907475069538318',
+                'edit_history_tweet_ids' => [
+                '1908907475069538318'
+                ]
+            ]
+            ]
+        ]);
 
         $jsonData = json_decode($testData, true);
 
@@ -61,11 +65,38 @@ class FormatApiTwitter implements FormatApiDataInterface
         return $this->getData($jsonData);
     }
 
-    public function getData(array $data): void {
+    public function getData(array $data): array {
+        //     0 => array:3 [▼
+        //       "text" => "lucina book"
+        //       "url" => "https://x.com/RotomDocs/status/1908907475069538317/photo/1"
+        //       "id" => "1908907475069538317"
+        //     ]
+        //     1 => array:3 [▼
+        //       "text" => "another tweet"
+        //       "url" => "https://t.co/example"
+        //       "id" => "1908907475069538318"
+        //     ]
+
         $formattedData = [];
-        foreach ($data as $item) {
+    
+        foreach($data['data'] as $tweet) {
+            if (!isset($tweet['text'])) {
+                continue;
+            }
             
+            $text = $tweet['text'];
+            preg_match('/(.+)\s+(https?:\/\/\S+)$/', $text, $matches);
+            
+            $formattedTweet = [
+                'id' => $tweet['id'],
+                'text' => $matches[1] ?? $text,
+                'image' => $matches[2] ?? null,
+            ];
+            
+            $formattedData[] = $formattedTweet;
         }
+   
         
+        return $formattedData;
     }
 }
