81a38ef1359759e60a0545b1a903736d16204886
Yanis-Ourir <ourir.yanis@gmail.com>
2025-04-09 22:18:55 +0200

    correct api reddit

--- src/Service/FormatApiReddit.php
+++ src/Service/FormatApiReddit.php
@@ -13,6 +13,17 @@ class FormatApiReddit implements FormatApiDataInterface
         $this->linkRepository = $linkRepository;
     }
 
+    public function ApiCall(string $link) {
+        $client = new \GuzzleHttp\Client();
+        // $response = $client->get($link, [
+        //     'headers' => [
+        //         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        //     ]
+        // ]);
+        // $data = json_decode($response->getBody(), true);
+        // return $this->format($data['data']['children'] ?? []);
+    }
+
     public function format(array $data): array
     {
         $formattedData = [];
